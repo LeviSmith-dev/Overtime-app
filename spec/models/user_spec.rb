94a141889bfe56fee9dc1cea6326988026f13342
Levi Smith <smith.levim@gmail.com>
2016-12-19 15:49:14 -0700

    setup some rspec tests and added posts

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Creation" do 
+  	before do
+  		@user = User.create(email: "test@test.com", password: "levi32", password_confirmation: "levi32", first_name: "levi", last_name: "smith")
+  		expect(@user).to be_valid
+  	end 
+
+  	it "cannot be created without first_name, Last_name" do
+  		@user.first_name = nil
+  		@user.last_name = nil
+  		expect(@user).to_not be_valid
+  	end
+  end
+end
