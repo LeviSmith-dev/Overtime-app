feb9525902babbcc689824e1231fa5bd2831be76
Levi Smith <smith.levim@gmail.com>
2017-01-05 15:53:55 -0700

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,9 @@ puts "1 Admin User created"
 
 
 
-AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
